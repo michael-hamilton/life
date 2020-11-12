17af4bb11c839ae389faffc61756d90066b1821b
Michael Hamilton <michaelhamilton626@gmail.com>
2020-11-11 21:19:38 -0500

    es6 and responsive

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# life
-Every developer should make a life clone at least once in their lives
+# [Life](http://miska.me/projects/life/index.html)
+An implementiton of Conway's Game of Life in Javascript.
+
+## Setup
+Run `npm i` to install dependencies, then run `npm run dev` to start a development server (likely at http://localhost:1234).
+Alternatively, `npm build` will bundle everything into `/dist`.
 
 ## Usage
-Simply view `life.html` in a web browser.
+* Click on squares to toggle cells on and off
+* Press space to play or pause the simulation
+* Press R to reset the simulation
+
+## License
+Released under the MIT license - do whatever you wish with it.
+
+## Acknowledgement
+Made with love by [Michael Hamilton](http://miska.me). 
--- life.js
+++ life.js
@@ -1,46 +1,37 @@
-var c = document.getElementById('canvas'),	//canvas element
-  ctx = c.getContext('2d'); //canvas context
+// Life
+// © 2015-2020 Michael Hamilton
 
-cells = [], //arr
-  h = w = 10, //dimensions of each cell (in px)
-  speed = 100, //number of ms between each draw
-  state = false, //current state of game (draw or update)
-  steps = 0; //current number of cycles
+const c = document.getElementById('canvas');//canvas element
+const ctx = c.getContext('2d'); //canvas context
+let cells = []; //arr
+let h = w = 12; //dimensions of each cell (in px)
+let state = false; //current state of game (draw or update)
+let steps = 0; //current number of cycles
+let speed = 10; //current number of cycles
 
-function init() {
-  for(i=0;i<c.height/h;i++) {
+c.height = window.innerHeight;
+c.width = window.innerWidth;
+
+const init = () => {
+  for (let i = 0; i < c.height / h; i++) {
     cells[i] = []
-    for(j=0;j<c.width/w;j++) {
+    for(let j = 0; j < c.width / w; j++) {
       cells[i][j] = 0;
     }
   }
 
-  //Toggle the cell at cursor position when canvas is clicked
-  c.addEventListener('click', function(e) {
-    var x = Math.floor(e.offsetX/w),
-      y = Math.floor(e.offsetY/h)
-    cells[y][x] = cells[y][x] ? 0 : 1;
-  });
-
-  //Start game when 'space' is pressed
-  window.addEventListener('keydown', function(e) {
-    if(e.keyCode == 32) state = state ? false : true;
-  });
-
   draw();
 }
 
-function update() {
+const update = () => {
   //Temporary array for state of cells in next cycle
-  var result = [];
+  const result = [];
 
-  function val(x, y) {
-    return cells[x] && cells[x][y];
-  }
+  const val = (x, y) => cells[x] && cells[x][y];
 
   //Returns amount of neighbor cells that are active
-  function check(x, y) {
-    var amount = 0;
+  const check = (x, y) => {
+    let amount = 0;
 
     if(val(x-1, y-1)) amount++;
     if(val(x, y-1)) amount++;
@@ -55,12 +46,12 @@ function update() {
   }
 
   //Check each cell and update the result array
-  cells.forEach(function(row, x) {
+  cells.forEach((row, x) => {
     result[x] = [];
-    row.forEach(function(cell, y) {
-      var neighbors = check(x, y);
-      if(cell) {
-        if(neighbors == 2 || neighbors == 3) {
+    row.forEach((cell, y) => {
+      let neighbors = check(x, y);
+      if (cell) {
+        if (neighbors === 2 || neighbors === 3) {
           result[x][y] = 1;
         }
         else {
@@ -68,7 +59,9 @@ function update() {
         }
       }
       else {
-        if(neighbors == 3) result[x][y] = 1;
+        if (neighbors === 3) {
+          result[x][y] = 1;
+        }
         else result[x][y] = 0;
       }
     });
@@ -81,13 +74,13 @@ function update() {
   draw();
 }
 
-function draw() {
+const draw = () => {
   //Clear last frame
   ctx.clearRect(0,0,c.width,c.height);
 
   //Draw the cells in the current cycle
-  cells.forEach(function(row, y) {
-    row.forEach(function(cell, x) {
+  cells.forEach((row, y) => {
+    row.forEach((cell, x) => {
       ctx.fillStyle = cell ? "#0a90ff" : '#d7d7d7';
       ctx.fillRect(x*w+1,y*h+1,w-1,h-1);
     });
@@ -95,14 +88,54 @@ function draw() {
 
   //Update step counter
   ctx.fillStyle = 'black';
+  ctx.fillRect(c.width - 200,0,c.width, 24);
   ctx.font = '18px sans-serif';
-  ctx.fillText('Steps: ' + steps, 0, 18);
+  ctx.fillText(`Space to play/pause`, 10, 20);
+  ctx.fillText(`R to reset`, 10, 40);
+  ctx.fillStyle = 'white';
+  ctx.font = '18px sans-serif';
+  ctx.fillText(`Generation: ${steps}`, c.width - 190, 18);
 
   //Update every n number of ms as defined by 'speed'
-  setTimeout(function() {
-    if(state) update();
-    else draw();
+  setTimeout(() => {
+    if (state) {
+      update();
+    }
+    else {
+      draw();
+    }
   }, speed);
 }
+
+//Toggle the cell at cursor position when canvas is clicked
+c.addEventListener('mousedown', function(e) {
+  const x = Math.floor(e.offsetX/w);
+  const y = Math.floor(e.offsetY/h);
+  cells[y][x] = cells[y][x] ? 0 : 1;
+});
+
+//Start game when 'space' is pressed
+window.addEventListener('keydown', function(e) {
+  console.log(e.code);
+  switch(e.code) {
+    case 'Space':
+      state = !state;
+      break;
+    case 'KeyR':
+      state = false;
+      steps = 0;
+      init();
+      break;
+  }
+});
+
+window.addEventListener('resize', () => {
+  c.height = window.innerHeight;
+  c.width = window.innerWidth;
+
+  state = false;
+  steps = 0;
+  init();
+});
 
 init();
